9873958d77d21c03da702ef8078acab701c0ea10
Vinicius Inocencio <vicnicius2009@gmail.com>
2022-07-01 20:40:36 -0300

    Feat: adding pomodoro-timer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import { PomodoroTimer } from './components/pomodoro-timer';
 
 function App() {
-  return <div className="App">Olá Mundo</div>;
+  return (
+    <div className="App">
+      <PomodoroTimer defaultPomodoroTime={1500} />
+    </div>
+  );
 }
 
 export default App;
