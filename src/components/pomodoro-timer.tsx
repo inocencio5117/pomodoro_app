89cea894af7509d6ac91a512884db846b326f6f1
Vinicius Inocencio <vicnicius2009@gmail.com>
2022-07-02 19:02:38 -0300

    Feat: adding sounds on play/stop

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,7 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import React, { useState, useEffect } from 'react';
 import { useInterval } from '../hooks/userInterval';
 import { Button } from './button';
 import { Timer } from './timer';
+const bellStart = require('../sounds/bell-start.mp3');
+const bellFinish = require('../sounds/bell-finish.mp3');
+
+const audioStartWorking = new Audio(bellStart);
+const audioStopWorking = new Audio(bellFinish);
+
+audioStopWorking.volume = 0.3;
+audioStartWorking.volume = 0.3;
 
 interface Props {
   defaultPomodoroTime: number;
@@ -34,6 +43,7 @@ export function PomodoroTimer(props: Props): JSX.Element {
     setWorking(true);
     setResting(false);
     setMainTime(props.defaultPomodoroTime);
+    audioStartWorking.play();
   };
 
   const configureRest = (long: boolean) => {
@@ -46,6 +56,8 @@ export function PomodoroTimer(props: Props): JSX.Element {
     } else {
       setMainTime(props.shortRestTime);
     }
+
+    audioStopWorking.play();
   };
 
   return (
